78deb4c5a4f375b9e546c4cc43f4fc13e1567634
Matheus Lima Messias <Matheus.Messias@ibm.com>
2022-05-17 16:31:35 -0300

    updated

---
+++ src/Navigator.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from "react";
+import { BackHandler } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import CreateNote from "./screens/CreateNote"
+import EditNote from "./screens/EditNote"
+import Home from "./screens/Home"
+import HomeNote from "./screens/HomeNote"
+import OpenNote from "./screens/OpenNote"
+
+
+
+export default () => {
+
+  const Stack = createNativeStackNavigator();
+
+  //useEffect(() => {
+  //  BackHandler.addEventListener("backPress", () => true);
+  //  return () => BackHandler.removeEventListener("backPress", () => true);
+  //}, []);
+
+  return (
+
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="CreateNote"
+          component={CreateNote}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="HomeNote"
+          component={HomeNote}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="OpenNote"
+          component={OpenNote}
+          options={{ headerShown: false }}
+        />  
+        <Stack.Screen
+          name="EditNote"
+          component={EditNote}
+          options={{ headerShown: false }}
+        />     
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
